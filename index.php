a577535ba4b2282e4b19557b116f4053903e3d5f
usjes1 <41988969+usjes1@users.noreply.github.com>
2018-08-18 01:11:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 <html>
 <head>
      <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="http://localhost/php/style.css">
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body id="target">
     <header>
-      <img src="http://localhost/gg.jpg" alt="gg">
-  <h1><a href="http://localhost/php/index.php">JavaScript</a></h1>
+      <img src="gg.jpg" alt="gg">
+  <h1><a href="index.php">JavaScript</a></h1>
     </header>
     <nav>
         <ol>
